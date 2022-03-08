2f25cfa1e6279b819c153f2b23ef18e9d9073522
Jeong Hwan Lee <40652753+joungh93@users.noreply.github.com>
2022-03-08 13:05:34 +0900

    Add files via upload

---
+++ read_vel.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 8 13:04:39 2022
+@author: jlee
+"""
+
+
+import numpy as np
+from matplotlib import pyplot as plt
+import glob, os
+from astropy.io import fits
+
+import warnings
+warnings.filterwarnings("ignore")
